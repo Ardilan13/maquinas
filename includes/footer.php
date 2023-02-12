5fd48884a63ce987ba0e6b57c7d714e263e50b37
Dilan Corredor <dilancorr@gmail.com>
2023-02-11 21:31:59 -0500

    ajustes iniciales proyecto

---
+++ includes/footer.php
@@ -0,0 +1,24 @@
+<footer>
+    <div class="ajuste"></div>
+    <p>&#169; 2023 Control Maquinas</p>
+    <div class="redes">
+        <a href="">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="35" height="35" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <rect x="4" y="4" width="16" height="16" rx="4" />
+                <circle cx="12" cy="12" r="3" />
+                <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+            </svg>
+        </a>
+        <a href="">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-whatsapp" width="35" height="35" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
+                <path d="M9 10a0.5 .5 0 0 0 1 0v-1a0.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a0.5 .5 0 0 0 0 -1h-1a0.5 .5 0 0 0 0 1" />
+            </svg>
+        </a>
+    </div>
+</footer>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php require_once 'includes/header.php'; ?>
+
+<main>
+
+</main>
+
+<section>
+
+</section>
+
+<?php require_once 'includes/footer.php'; ?>
