8e965d9c36fc6e88436559c01369ab7f2bcecc1b
sebanma <Sebas13david21@gmail.com>
2023-02-12 21:31:27 -0500

    mods
    Modulos generados.

--- menu.php
+++ menu.php
@@ -6,13 +6,13 @@
             <p>Menu Principal</p>
         </div>
         <div class="info menu_principal">
-            <a class="mp_btn" href="pages/.php">Maquinas</a>
-            <a class="mp_btn" href="pages/.php">Inventario</a>
-            <a class="mp_btn" href="pages/.php">Solicitudes de Servicio</a>
-            <a class="mp_btn" href="pages/.php">Ordenes de Trabajo</a>
-            <a class="mp_btn" href="pages/.php">Prestamos</a>
-            <a class="mp_btn" href="pages/.php">Tareas</a>
-            <a class="mp_btn" href="pages/.php">Calendario</a>
+            <a class="mp_btn" href="mod_maquina.php">Maquinas</a>
+            <a class="mp_btn" href="mod_inventario.php">Inventario</a>
+            <a class="mp_btn" href="mod_solicitud.php">Solicitudes de Servicio</a>
+            <a class="mp_btn" href="mod_orden.php">Ordenes de Trabajo</a>
+            <a class="mp_btn" href="mod_prestamo.php">Prestamos</a>
+            <a class="mp_btn" href="mod_tarea.php">Tareas</a>
+            <a class="mp_btn" href="mod_calendario.php">Calendario</a>
             <a id="cerrar_sesion" href="ajax/logout.php">Cerrar Sesion</a>
         </div>
     </div>
---
+++ mod_inventario.php
@@ -0,0 +1,13 @@
+<?php require_once 'includes/header.php'; ?>
+
+<main>
+    <div class="container">
+        <div class="header">
+            <p>Inventario</p>
+        </div>
+        
+    </div>
+</main>
+
+
+<?php require_once 'includes/footer.php'; ?>
