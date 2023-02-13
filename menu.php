d6787836615a24930f162489e2a3a1e2f80d56b3
Dilan Corredor <dilancorr@gmail.com>
2023-02-12 19:24:07 -0500

    Creacion menu principal
    modulo menu principal, creacion carpeta pages, creacion scss respectivo

--- menu.php
+++ menu.php
@@ -1,5 +1,21 @@
 <?php require_once 'includes/header.php'; ?>
 
-<h1>Prueba</h1>
+<main>
+    <div class="container">
+        <div class="header">
+            <p>Menu Principal</p>
+        </div>
+        <div class="info menu_principal">
+            <a class="mp_btn" href="pages/.php">Maquinas</a>
+            <a class="mp_btn" href="pages/.php">Inventario</a>
+            <a class="mp_btn" href="pages/.php">Solicitudes de Servicio</a>
+            <a class="mp_btn" href="pages/.php">Ordenes de Trabajo</a>
+            <a class="mp_btn" href="pages/.php">Prestamos</a>
+            <a class="mp_btn" href="pages/.php">Tareas</a>
+            <a class="mp_btn" href="pages/.php">Calendario</a>
+            <a id="cerrar_sesion" href="ajax/logout.php">Cerrar Sesion</a>
+        </div>
+    </div>
+</main>
 
 <?php require_once 'includes/footer.php'; ?>
